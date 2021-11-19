916905730d0761f7b3208dd436e335ad50f78e5d
SaurabhN16 <91530878+saurabhnimkande@users.noreply.github.com>
2021-11-19 21:47:17 +0530

    added new middleware

--- app.js
+++ app.js
@@ -2,38 +2,70 @@ const express = require('express');
 const app = express();
 const data=require('./data')
 
+var ans=[];
+
 app.use(express.json());
 
+const addName = (req,res,next) => {
+    console.log("running");
+    ans=[];
+    next();
+    if(ans.length==1) {
+        res.send({api_requested_by:"Saurabh Nimkande",books:ans[0]})
+    } else {
+        res.send({api_requested_by:"Saurabh Nimkande",books:ans})
+    }
+    console.log("done")
+  };
+  
+
+app.use(addName);
+
+
 app.get("/", (req, res) => {
-    res.send(data);
+    new_arr=[...data]
+    ans=[...new_arr];
 });
 
 app.post("/books", (req, res) => {
    const new_arr=[req.body,...data];
-   res.send(new_arr);
+   ans=[...new_arr];
 });
 
 app.get("/books/:year", (req, res) => {
-    const newData=data.filter( (user)=> user.year == req.params.year);
-    res.send(newData);
+    const new_arr=data.filter( (user)=> user.year == req.params.year);
+    ans=[...new_arr];
 });
 
 app.patch("/books/:year", (req, res) => {
-    const newData=data.filter( (user)=> {
+    const new_arr=data.filter( (user)=> {
         if(user.year == req.params.year) {
             user.year=2022;
             user.author="Saurabh";
             return user;
         }
     });
-    res.send(newData);
+    ans=[...new_arr];
 })
 
 app.delete("/books/:year", (req, res)=>{
-    const newarr=data.filter( (user)=> user.year!=req.params.year);
-    res.send(newarr);
+    const new_arr=data.filter( (user)=> user.year!=req.params.year);
+    ans=[...new_arr];
 })
 
 app.listen(2525, ()=>{
     console.log("listening on port 2525")
 });
+
+// const authorise = (permission) => {
+//     return (req, res, next) => {
+//       const originalSendFunc = res.send.bind(res);
+//       res.send = function (body) {
+//         body.name = "Nrupul Dev";
+//         console.log(body); // do whatever here
+//         return originalSendFunc(body);
+//       };
+//       next();
+//     };
+//   };
+  
